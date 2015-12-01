c07a14bb46e886154b9cc0fa8906e2ee6d26aa2d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2015-12-01 21:34:12 +0100

    Make OCAMLROOT available in suitable form to Cygwin bash

--- install_ocaml.cmd
+++ install_ocaml.cmd
@@ -1,8 +1,9 @@
 REM Download and install OCaml and flexlink (unless it was already done).
-REM Prepare the environment variables,... to use it.
+REM Prepare the environment variables,... to use it.  OCaml is installed
+REM at %OCAMLROOT%
 REM
 REM If you are using Cygwin, install it in C:\cygwin first and then
-REM execute this script.
+REM execute this script.  Execute bash with the option "-l".
 
 set OCAMLROOT=%PROGRAMFILES%/OCaml
 
@@ -27,10 +28,15 @@ set CYGWINBASH=C:\cygwin\bin\bash.exe
 
 if exist %CYGWINBASH% (
   REM Make sure that "link" is the MSVC one and not the Cynwin one.
-  echo VCPATH="`cygpath -p '%Path%'`" > C:\cygwin\tmp\msenv
+  echo VCPATH="`cygpath -u -p '%Path%'`" > C:\cygwin\tmp\msenv
   echo PATH="$VCPATH:$PATH" >> C:\cygwin\tmp\msenv
   %CYGWINBASH% -lc "tr -d '\\r' </tmp/msenv > ~/.msenv64"
   %CYGWINBASH% -lc "echo '. ~/.msenv64' >> ~/.bash_profile"
+  REM Make OCAMLROOT available in Unix form:
+  echo OCAMLROOT_WIN="`cygpath -m '%OCAMLROOT%'`" > C:\cygwin\tmp\env
+  (echo OCAMLROOT="`cygpath -u \"$OCAMLROOT_WIN\"`") >>C:\cygwin\tmp\env
+  echo export OCAMLROOT_WIN OCAMLROOT >>C:\cygwin\tmp\env
+  %CYGWINBASH% -lc "tr -d '\\r' </tmp/env >> ~/.bash_profile"
 )
 
 set <NUL /p=Ready to use OCaml & ocamlc -version
